c6545366bb8dc6a86c07f1a55a0dad4915ff079c
Karimov <devizzatillo@gmail.com>
2025-09-18 17:55:47 +0500

    adding functions

--- main.py
+++ main.py
@@ -4,15 +4,27 @@ from environs import Env
 env = Env() 
 env.read_env() 
 
-mydb = connect(
-	user = env.str("DB_USER"),
-	password = env.str("DB_PASSWORD"),
-	dbname = env.str("DB_NAME"),
-	host = env.str("DB_HOST"),
-	port = env.str("DB_PORT")
+def get_connect(): 
 
+	return  connect( 
+		user = env.str("DB_USER"),
+		password = env.str("DB_PASSWORD"),
+		dbname = env.str("DB_DATABASE"),
+		host = env.str("DB_HOST"),
+		port = env.str("DB_PORT")
 )
 
-
-
-print("DATABASE connected succesfully")  
+def query_sql(query,*args , fetchone=False, fetchall=False, commit=False):
+	try:
+		with get_connect() as db:
+			db.autocommit = True
+			with db.cursor() as dbc: 
+				dbc.execute(query,args or ())
+				if fetchone:
+					return dbc.fetchone() 
+				if fetchall: 
+					return dbc.fetchall() 
+			if commit:
+				return True 
+	except:
+		return None 
